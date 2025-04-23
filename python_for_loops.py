1f3e3d03dddb22c82a6bdee81eb4d676fe29fb7a
James Hawk <jph94880@gmail.com>
2025-04-22 22:51:22 -0500

    Makes changes

--- python_for_loops.py
+++ python_for_loops.py
@@ -4,4 +4,78 @@
 
 # With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
 
+string = "A foolish consistancy is the hobgoblin of a little mind."
+
+# for letter in string:
+#     print(letter)
+
+cars = ["Ford", "Chevy", "Jeep"]
  
+
+#  Break statements terminate a loop BEFORE a matching codition is found. 
+for car in cars:
+    if car == "Chevy":
+        break
+    print(car)
+
+instruments = ["piano", "guitar", "saxophone"]
+
+for instrument in instruments: 
+    # if instrument == "guitar":
+    #     continue
+    print("Instrument:           ", instrument)
+
+# To loop through a set of code a specified number of times, we can use the range() function,
+
+# The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
+
+for x in range(6):
+    print(x)
+
+print("__________________________________________")
+
+for y in range(2, 6):
+    print(y)
+
+# The range() function defaults to increment the sequence by 1, however it is possible to specify the increment value by adding a third parameter
+print("__________________________________________")
+
+
+# The increment value is the last argumen tto be passed into the range() method. 
+for x in range(2, 30, 3):
+    print(x)
+else: 
+    print("Finished!")
+
+print("__________________________________________")
+
+for x in range(6):
+    if x == 3:
+        break
+    print(x)
+else: 
+    print("Finally finished!")
+
+# Nested Loops
+# The "inner loop" will be executed one time for each iteration of the "outer loop
+
+print("__________________________________________")
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
+
+print("__________________________________________")
+
+singles = [1,2,3]
+tens = [1,2,3]
+
+for x in tens:
+    for y in singles:
+        print( x, y)
+
+for letter in "string":
+    pass
---
+++ python_functions.py
@@ -0,0 +1,41 @@
+import random
+
+# By default, a function must be called with the correct number of arguments. Meaning that if your function expects 2 arguments, you have to call the function with 2 arguments, not more, and not less.
+
+
+
+def my_function(first_name, last_name):
+    # print(first_name + " " + last_name)
+    print(f'{first_name} {last_name}')
+
+my_function("James", "Hawk")
+my_function("John", "Doe")
+my_function("Gerorge", "Washington")
+
+terminal_line_break = "__________________________________"
+
+print(terminal_line_break)
+
+def sentence_function(*kids):
+    print("The youngest child is " + kids[3])
+
+
+# pass any number of arguments into sentence_function. The function will index the argument values like a list. 
+
+sentence_function("James", "John", "Julie", "Jarred", "Jason")
+
+print(terminal_line_break)
+
+# You can also send values with *key = value* syntax. 
+
+def new_func(child3, child2, child1):
+    print("The youngest child is " + child3)
+
+# Passing it too few values results in a TypeError. 
+
+# new_func(child3="Timmy")
+
+# PAss in all arguments into the function call that are passed into the function definition,even if the values go unused. 
+
+new_func(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
